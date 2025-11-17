310e5565a9fb1e2d1201779a93e29c535b64ee30
ZaydenMartires <zaydenm3@nycstudents.net>
2025-11-17 08:32:42 -0500

    Initial commit

---
+++ src/GymApp.java
@@ -0,0 +1,9 @@
+public class GymApp
+{
+public int Calorie (int poundWeight)
+{
+    int ogCalCount= poundWeight * 3500;
+    return ogCalCount;
+}
+
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("BodyWeight(Nearest lb)");
+        int ogBodyweight = scanner.nextInt();
+        System.out.println("Exercise Daily(1=low,2=moderate,3=high)");
+        int exercise =  scanner.nextInt();
+
+
+
+    }
+}
